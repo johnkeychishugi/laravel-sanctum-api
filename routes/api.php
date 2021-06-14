1620f1cc0c425b02359741d65c2dd403f3afdc00
John Chishugi <jkchishugi@gmail.com>
2021-06-15 00:14:28 +0100

    ft-group-auth-routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -15,13 +16,26 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//Public Routes
+// Route::resource('products', ProductController::class);
 
-Route::resource('products',ProductController::class);
+Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login',[AuthController::class, 'login']);
+Route::get('/products',[ProductController::class ,'index']);
+Route::get('/products/{id}',[ProductController::class ,'show']);
 Route::get('/products/search/{name}',[ProductController::class ,'search']);
 
-// Route::get('/product/{id}',[ProductController::class ,'show']);
-// Route::post('/products',[ProductController::class, 'store']);
+//Protected Routes
+Route::group(['middleware' => ['auth:sanctum']] ,function () {
+    Route::post('/products',[ProductController::class, 'store']);
+    Route::put('/products/{id}',[ProductController::class, 'update']);
+    Route::delete('/products/{id}',[ProductController::class, 'destroy']);
+    Route::post('/logout',[AuthController::class, 'logout']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
 });
+
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
