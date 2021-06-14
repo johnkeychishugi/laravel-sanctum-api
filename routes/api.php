6415dd21470a01f957ed6f932679ed37ac101b3a
John Chishugi <jkchishugi@gmail.com>
2021-06-14 21:43:57 +0100

    ft-test-api-endpoint

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/products',function(){
+   return 'Products';
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
