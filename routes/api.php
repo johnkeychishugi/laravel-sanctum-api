1b031eebe590f1e045ce0dc8f2d3d3e09a4c78f0
John Chishugi <jkchishugi@gmail.com>
2021-06-14 22:04:48 +0100

    ft-add-post-product-route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +16,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/products',function(){
-   return 'Products';
+   return  Product::all();
 });
+
+Route::post('/products',function(){
+    return Product::create([
+            'name' => 'Product One',
+            'slug' => 'product-one',
+            'description' => 'This is product one',
+            'prix' => '99.99'
+    ]);
+ });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
