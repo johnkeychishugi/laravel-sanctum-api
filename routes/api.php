cdcec8618fd223df45963dcf809c6533832267aa
John Chishugi <jkchishugi@gmail.com>
2021-06-14 22:14:35 +0100

    ch-route-change

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Product;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,18 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products',function(){
-   return  Product::all();
-});
+Route::get('/products',[ProductController::class ,'index']);
 
-Route::post('/products',function(){
-    return Product::create([
-            'name' => 'Product One',
-            'slug' => 'product-one',
-            'description' => 'This is product one',
-            'prix' => '99.99'
-    ]);
- });
+Route::post('/products',[ProductController::class, 'store']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
