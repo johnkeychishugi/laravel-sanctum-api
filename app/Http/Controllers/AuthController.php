ef5c0a8d11bf0006bd8b596083e106ccf5122609
John Chishugi <jkchishugi@gmail.com>
2021-06-15 00:13:58 +0100

    ft-add-auth-with-sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fiels = $this->validate($request,[
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fiels['name'],
+            'email' => $fiels['email'],
+            'password' => bcrypt($fiels['password'])
+
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fiels = $this->validate($request,[
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+         $user = User::where('email',$fiels['email'])->first();
+
+         if(!$user || !Hash::check($fiels['password'], $user->password)){
+            return response([
+                'message' => 'Bed creds'
+            ],401);
+         }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 200);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged out'
+        ];
+    }
+}
