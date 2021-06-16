63294f12daca2fdbc02e49c267a966dcdf19fb75
John Chishugi <jkchishugi@gmail.com>
2021-06-16 17:36:29 +0100

    ft-add-router-swagger-docs

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('api/api-docs', function () {
+    return view('swagger.index');
+});
