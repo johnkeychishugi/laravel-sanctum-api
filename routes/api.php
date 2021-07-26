3613f65dfb4bf3a176f1fdd26944114e1d72f543
John Chishugi <jkchishugi@gmail.com>
2021-07-26 10:19:09 +0100

    ft-code-style

--- routes/api.php
+++ routes/api.php
@@ -6,16 +6,6 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 //Public Routes
 // Route::resource('products', ProductController::class);
 
